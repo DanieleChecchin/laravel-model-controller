52f9ea127a7defa22867b6607c56944526623b69
DanieleChecchin <danielechecchin26@gmail.com>
2024-10-24 15:24:08 +0200

    esercizio completato

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
-    public function index(){
-        return view('movies.index');
+        public function index(){
+        $movies = Movie::all();
+        return view('movies.index', compact('movies'));
     }
 }
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts.app')
 
-@section('page-title', 'Homepage')
+@section('page-title', 'Movies Index')
 
 @section('main-content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="fw-bold text-center my-5">
+                    Movies Index
+                </h1>
+            </div>
+            @forelse ($movies as $movie)
+                <div class="col-6">
+                    <div class="card my-3">
+                        <div class="card-body">
+                            <h5 class="card-title"> {{ $movie->title }} </h5>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">{{ $movie->original_title }}</li>
+                            <li class="list-group-item">{{ $movie->nationality }}</li>
+                            <li class="list-group-item">{{ $movie->date }}</li>
+                            <li class="list-group-item">{{ $movie->vote }}</li>
+                        </ul>
+                    </div>
+                </div>
+            @empty
+                <div class="col-12">
+                    <h2>Nessun film disponibile al momento</h2>
+                </div>
+            @endforelse
+        </div>
+    </div>
 
 @endsection
